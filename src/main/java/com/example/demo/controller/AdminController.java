3b2d0154ef73fec2acd57cc48b3e1f7bd7f5619f
ayushgirulkar <girulkarayush@gmail.com>
2026-05-18 22:46:37 +0530

    Create AdminController.java

---
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/all-users")
+    public ResponseEntity<List<User>> getAllUsers()
+    {
+        List<User> all = userService.getAll();
+        if (all!=null && !all.isEmpty())
+        {
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(all, HttpStatus.NOT_FOUND);
+    }
+}
