7b1844a93fe66a100911a35e8cee7af0f93e7b9d
ayushgirulkar <girulkarayush@gmail.com>
2026-05-19 22:51:31 +0530

    Update UserServiceTests.java

--- src/test/java/com/example/demo/service/UserServiceTests.java
+++ src/test/java/com/example/demo/service/UserServiceTests.java
@@ -1,4 +1,14 @@
 package com.example.demo.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 public class UserServiceTests {
+
+    @Test
+    public void testing()
+    {
+        assertEquals(4,2+1);
+    }
+
 }
