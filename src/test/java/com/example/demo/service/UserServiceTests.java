0d1998b54f8a84eefeeace2768d3f231fc87cc94
ayushgirulkar <girulkarayush@gmail.com>
2026-05-19 23:00:05 +0530

    Update UserServiceTests.java

--- src/test/java/com/example/demo/service/UserServiceTests.java
+++ src/test/java/com/example/demo/service/UserServiceTests.java
@@ -1,14 +1,31 @@
 package com.example.demo.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import com.example.demo.reposiratory.UserReposiratory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class UserServiceTests {
 
+    @Autowired
+    UserReposiratory userReposiratory;
+
     @Test
-    public void testing()
+    public void testFindByUserName()
     {
-        assertEquals(4,2+1);
+
+        assertNotNull(userReposiratory.findByUserName("ayush"));
     }
+
+    @Test
+    public void testMarks()
+    {
+        assertEquals(40,15+25);
+    }
+
 
 }
