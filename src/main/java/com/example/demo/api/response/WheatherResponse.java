5c05c74b6627dc5dfa950733d6185cce18cc5f39
ayushgirulkar <girulkarayush@gmail.com>
2026-05-26 22:34:10 +0530

    Update WheatherResponse.java

--- src/main/java/com/example/demo/api/response/WheatherResponse.java
+++ src/main/java/com/example/demo/api/response/WheatherResponse.java
@@ -1,71 +1,22 @@
 package com.example.demo.api.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
 public class WheatherResponse
 {
-    public Request request;
-    public Location location;
+   
     public Current current;
 
-
-    public class AirQuality{
-        public String co;
-        public String no2;
-        public String o3;
-        public String so2;
-        public String pm2_5;
-        public String pm10;
-        public String us-epa-index;
-        public String gb-defra-index;
-    }
-
-    public class Astro{
-        public String sunrise;
-        public String sunset;
-        public String moonrise;
-        public String moonset;
-        public String moon_phase;
-        public int moon_illumination;
-    }
-
     public class Current{
-        public String observation_time;
         public int temperature;
-        public int weather_code;
-        public ArrayList<String> weather_icons;
-        public ArrayList<String> weather_descriptions;
-        public Astro astro;
-        public AirQuality air_quality;
-        public int wind_speed;
-        public int wind_degree;
-        public String wind_dir;
-        public int pressure;
-        public int precip;
-        public int humidity;
-        public int cloudcover;
-        public int feelslike;
-        public int uv_index;
-        public int visibility;
-        public String is_day;
+        @JsonProperty("weather_description")
+        public List<String> weatherDescriptions;
+
     }
 
-    public class Location{
-        public String name;
-        public String country;
-        public String region;
-        public String lat;
-        public String lon;
-        public String timezone_id;
-        public String localtime;
-        public int localtime_epoch;
-        public String utc_offset;
-    }
 
-    public class Request{
-        public String type;
-        public String query;
-        public String language;
-        public String unit;
-    }
 }
 
 
