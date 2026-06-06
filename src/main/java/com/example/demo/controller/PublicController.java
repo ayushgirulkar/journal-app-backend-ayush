f6e65fd34f634b194ba9a05f2c9517b809cbb2a7
ayushgirulkar <girulkarayush@gmail.com>
2026-06-06 22:59:04 +0530

    Add JWT authentication to public controller
    Introduce an authentication flow for the public endpoints: add UserDetailsServiceImpl, AuthenticationManager, JwtUtil and logging to PublicController. The /create-user2 endpoint now authenticates credentials, returns a JWT on success (ResponseEntity<String>) and logs/returns a 400 on failure. Also add a placeholder generateToken method in JwtUtil (to be implemented).

--- src/main/java/com/example/demo/controller/PublicController.java
+++ src/main/java/com/example/demo/controller/PublicController.java
@@ -1,17 +1,35 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.User;
+import com.example.demo.service.UserDetailsServiceImpl;
 import com.example.demo.service.UserService;
+import com.example.demo.utils.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@Slf4j
 @RequestMapping("/public")
 public class PublicController {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    private  UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private  JwtUtil jwtUtil;
 
     @PostMapping("/create-user1")//signup
     public void createUser(@RequestBody User user)
@@ -19,8 +37,24 @@ public class PublicController {
         userService.saveNewUser(user);
     }
     @PostMapping("/create-user2")//login
-    public void createUser(@RequestBody User user)
+    public ResponseEntity<String> createUser(@RequestBody User user)
     {
-        userService.saveNewUser(user);
+        try
+        {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(user.getUserName(),user.getPasswords())
+            );
+            UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
+            String jwt= jwtUtil.generateToken(userDetails.getUsername());
+
+            return new ResponseEntity<>(jwt, HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            log.error("Exception occur while crateAuthenticationToken",e);
+            return  new ResponseEntity<>("Incorrect username or Passwords",HttpStatus.BAD_REQUEST);
+
+        }
+
     }
 }
