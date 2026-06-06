908fb94277343c2c5b190fa8260d671165ef7af0
ayushgirulkar <girulkarayush@gmail.com>
2026-06-06 22:44:20 +0530

    Add JWT deps, JwtUtil and controller endpoints
    Add JJWT dependencies (jjwt-api, jjwt-impl, jjwt-jackson) to pom.xml to enable JWT processing. Introduce a JwtUtil @Component placeholder class. Modify PublicController to expose two POST endpoints (/create-user1 for signup and /create-user2 for login) that currently call userService.saveNewUser(user). Note: the controller currently contains two methods with identical signatures and will need refactoring to implement distinct signup/login behavior and to resolve the duplicate method signature.

--- src/main/java/com/example/demo/controller/PublicController.java
+++ src/main/java/com/example/demo/controller/PublicController.java
@@ -5,6 +5,7 @@ import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/public")
 public class PublicController {
@@ -12,7 +13,12 @@ public class PublicController {
     @Autowired
     UserService userService;
 
-    @PostMapping("/create-user")
+    @PostMapping("/create-user1")//signup
+    public void createUser(@RequestBody User user)
+    {
+        userService.saveNewUser(user);
+    }
+    @PostMapping("/create-user2")//login
     public void createUser(@RequestBody User user)
     {
         userService.saveNewUser(user);
